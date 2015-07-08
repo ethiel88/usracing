b2a1ac4bc9014524a8465dc58066706316446296
Luis Villegas <ethiel88@gmail.com>
2015-07-08 08:01:51 -0400

    Setting base for crawlers

---
+++ new/cron_kd_926_odds.V2.php
@@ -0,0 +1,195 @@
+<?php 
+define("ALWAYSWRITE", FALSE); //always write eveng with the message "please check back shortly"
+define("OUTPUTFILEPATH", "./data.html"); //the absolute path of the file to be written
+$IdLeague = "438";
+
+$data = array();
+$title = "";
+$ret = gen($IdLeague, $data, $title);
+//echo $title;
+//print_r($data);
+gen2($data, $title);
+
+
+function gen($IdLeague, &$data, &$mainTitle){
+	$ret = false;
+	$mainTitle = "";
+	$data = array();
+	$xml_string = file_get_contents("./odds.xml"); 
+	$matches = array();
+	preg_match('/<league IdLeague="'.$IdLeague.'"[^>]*>(.*?)<\/league>/', $xml_string, $matches);
+	if(count($matches)<=0)return $ret;
+
+	$league = $matches[1];
+	$leagueObj = simplexml_load_string($matches[0]);
+	$mainTitle = (string) $leagueObj["Description"];
+
+	$sections = preg_split("/<banner[^>]*>/", $league); // split by sections
+	if(count($sections)<=0) return $ret;
+
+	$banners = array();
+	preg_match_all("/<banner[^>]*>/", $league, $banners); // for getting the banners
+	if(count($banners)<=0) return $ret;
+	$banners = $banners[0];
+
+	for($i=0; $i< count($banners); $i++) {
+		$bannerParams =  simplexml_load_string($banners[$i]);
+		$section = $sections[$i+1];
+		$games = array();
+
+		if($section != ""){
+			$g = array();
+			preg_match_all("/<game[^>]*>.*?<\/game>/", $section, $g);
+
+			foreach($g[0] as $gamesPlain){
+				$linesPlain = array();
+				preg_match_all("/<line[^>]*>/", $gamesPlain, $linesPlain);
+				$lines = array();
+
+				foreach ($linesPlain[0] as $line) {
+					$lineParams = simplexml_load_string($line);
+					$lines[] = array(
+						"tmname" => (string)$lineParams[0]["tmname"],
+						"odds" => (string)$lineParams[0]["odds"],
+						"oddsh" => (string)$lineParams[0]["oddsh"]
+						);
+				}
+			
+				$gameObj = simplexml_load_string("<document>".$gamesPlain."</document>");
+				$games[] = array(
+				"vtm" => (string)$gameObj->game["vtm"],
+				"htm" => (string)$gameObj->game["htm"],
+				"lines" => $lines
+				);
+			}
+		}
+
+		$data[] = array(
+			"ab" => (string) $bannerParams[0]["ab"],
+			"vtm" => (string) $bannerParams[0]["vtm"],
+			"htm" => (string) $bannerParams[0]["htm"],
+			"games" => $games
+			);
+	}
+	
+	return true;
+}
+
+function gen2($data, $title){
+	$output = "";
+	if(count($data)<=0) return write($output);
+
+	$firstHeader = false;
+	ob_start();
+?>
+<div class="table-responsive">
+    <table  border="1" class="data table table-condensed table-striped table-bordered" width="100%" cellpadding="0" cellspacing="0" border="0"  summary="<?php echo $title; ?>">
+        <caption><?php echo $title; ?></caption>
+        <tbody>
+<?php
+	foreach ($data as $d) {
+?>
+		<tr>   
+            <th colspan="3" class="center">
+            <?php
+            echo $d["vtm"];
+            if($d["htm"] != "") echo "<br />" . $d["htm"];
+            ?>
+            </th>
+        </tr>
+<?php
+		if(count($d["games"])>0){
+			foreach($d["games"] as $g){
+				if(count($g["lines"])>0){
+					if($g["htm"]!="")
+						echo '<tr><th colspan="3">'.$g["htm"].'</th></tr>';
+					if(!$firstHeader){
+						echo '<tr><th>Team</th><th>American Odds</th><th>Fractional Odds</th></tr>';
+						$firstHeader = true;
+					}
+					foreach($g["lines"] as $l){
+						if($l["tmname"]!="")$team = $l["tmname"];
+						else $team = $g["vtm"] . " " . $g["htm"];
+						echo '<tr>';
+						echo '<td>'.$team.'</td>';
+						echo '<td>'.$l["oddsh"].'</td>';
+						echo '<td>'.fractional($l["oddsh"]).'</td>';
+						echo '</tr>';
+					}
+				}
+			}
+		}
+	}
+?>
+        	<tr>      
+                <td class="dateUpdated center" colspan="5"><em>Updated <?php echo date("F j, Y");?>. Bet US Racing - Official <a href="http://www.usracing.com/kentucky-derby/odds">Kentucky Derby Odds</a>. <br>All odds are fixed odds prices. </em> </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<?php
+	$output = ob_get_clean();
+	return write($output);
+}
+
+function fractional($oddsh){
+    $oodsh_val = intval($oddsh);
+    $fractional = 0;
+
+    if(intval($oodsh_val)>0){
+       $fractional  = float2rat($oodsh_val/100);     
+    } else if($oodsh_val<0) {
+        $fractional  = float2rat(100/(-$oodsh_val));
+    } else {
+        $fractional  = $oddsh;   
+    }
+
+    return $fractional;
+}
+
+function float2rat($n, $tolerance = 1.e-6) {
+    $h1=1; $h2=0;
+    $k1=0; $k2=1;
+    $b = 1/$n;
+    do {
+        $b = 1/$b;
+        $a = floor($b);
+        $aux = $h1; $h1 = $a*$h1+$h2; $h2 = $aux;
+        $aux = $k1; $k1 = $a*$k1+$k2; $k2 = $aux;
+        $b = $b-$a;
+    } while (abs($n-$h1/$k1) > $n*$tolerance);
+
+    return "$h1/$k1";
+}
+
+function write($contents){
+	if($contents == ""){
+		$contents = "The odds are currently being updated, please check back shortly. <em> - Updated ".date("F j, Y").". </em>";
+		if(ALWAYSWRITE){
+			if(file_put_contents(OUTPUTFILEPATH, $contents)){
+				echo "File " , OUTPUTFILEPATH . " was written with no data.\n";
+				return true;
+			}
+			else{
+				echo "Error while trying to write file " . OUTPUTFILEPATH . " with no data. Please check permissions.\n";
+				return false;
+			}
+		}
+		else{
+			echo "Empty output, file was not written.\n";
+			return false; 
+		}
+	}
+	else{
+		if(file_put_contents(OUTPUTFILEPATH, $contents)){
+			echo "File " , OUTPUTFILEPATH . " was written.\n";
+			return true;
+		}
+		else{
+			echo "Error while trying to write file " . OUTPUTFILEPATH . ". Please check permissions.\n";
+			return false;
+		}
+	}
+
+}
+
---
+++ old/cron_live_in_running.php
@@ -0,0 +1,219 @@
+<?php
+if(isset($_REQUEST['IdLeague']) && strcasecmp(trim($_REQUEST['IdLeague']),"")!==0) { $argv[1] = $_REQUEST['IdLeague']; }
+if(isset($_REQUEST['BannerAbTrue']) && strcasecmp(trim($_REQUEST['BannerAbTrue']),"")!==0) { $argv[2] = $_REQUEST['BannerAbTrue']; }
+if(isset($_REQUEST['BannerAbFalse']) && strcasecmp(trim($_REQUEST['BannerAbFalse']),"")!==0) { $argv[3] = $_REQUEST['BannerAbFalse']; }
+if(isset($_REQUEST['SectionTitle']) && strcasecmp(trim($_REQUEST['SectionTitle']),"")!==0) { $argv[4] = $_REQUEST['SectionTitle']; }
+
+$banner_ab_true = "Off";
+$banner_ab_false = "Off";
+
+
+if(isset($argv[1]) AND strcasecmp($argv[1], intval($argv[1])) == 0){
+    $id_league = $argv[1];
+}
+
+if(isset($argv[2])){
+    $banner_ab_true = $argv[2];            
+}
+
+if(isset($argv[3])){
+    $banner_ab_false = $argv[3];        
+}
+
+if(isset($argv[4])){    
+    $section_title = $argv[4];        
+}
+
+$id_league = "906";
+$sections = array("AMERICAN PHAROAH WINS", "3 YEAR OLD SEASON UNDEFEATED", "AMERICAN PHAROAH NEXT RACE");
+$OutputPathFile = "/home/ah/allhorse/public_html/misc/odds_live_in_running.php";
+
+
+generateOuput($id_league, $OutputPathFile, $banner_ab_true, $banner_ab_false, $sections);
+
+
+
+
+function generateOuput($id_league, $OutputPathFile, $banner_ab_true = "Off", $banner_ab_false = "Off", $sections = array() ){
+    $string = file_get_contents("http://ww1.betusracing.ag/odds.xml"); 
+    $xml = new SimpleXMLElement($string);
+    $global_results = array();
+
+
+    $banners = $xml->xpath('/xml/league[@IdLeague="'.$id_league.'"]/banner');
+    $banners_vtm = array();
+    $banners_htm = array();
+    $k = 0;
+
+    foreach($banners as $b){
+        $banners_vtm[$k] = (string)$b->attributes()['vtm'];
+        $banners_htm[$k] = (string)$b->attributes()['htm'];
+        ++$k;
+    }
+
+    $games = $xml->xpath('/xml/league[@IdLeague="'.$id_league.'"]/game');
+    $index_banner = 1;
+    $captions = array();
+    $captions["title"] = array( 
+        "vtm" => ucwords(strtolower($banners_vtm[0])), 
+        "htm" => ucwords(strtolower($banners_htm[0])) );
+
+    foreach($games as $g){
+        $c = trim((string)$g->attributes()['vtm']);
+
+        if(!isset($captions[$c])){
+            $captions[$c] = array( 
+                "vtm" => ucwords(strtolower($banners_vtm[$index_banner])), 
+                "htm" => ucwords(strtolower($banners_htm[$index_banner])) );
+            ++$index_banner;
+        }
+    }
+
+
+    foreach ($sections as $title) {
+        $result_t = $xml->xpath('/xml/league[@IdLeague="'.$id_league.'"]/game[@vtm="'.$title.'"]');
+        
+        if(count($result_t) > 0){
+            $global_results[$title] = $result_t;
+        }
+
+        unset($result_t);
+    }
+
+    //$result = $xml->xpath('/xml/league[@IdLeague="'.$id_league.'"]/game[@vtm="'.$section_title.'"]');
+    $parent_node = $xml->xpath('/xml/league[@IdLeague="'.$id_league.'"]');
+
+    if(isset($parent_node) AND count($parent_node)>0){
+        $parent_node_attributes = $parent_node[0]->attributes();    
+    }
+
+    ob_start();
+    $result_count = count($global_results);
+
+    if(count($global_results)>0){
+        $table_title = $parent_node_attributes['Description'];
+        $array_contenders = array();
+        ?>
+        <div class="table-responsive">
+            <table  class="data table table-condensed table-striped table-bordered" width="100%" cellpadding="0" cellspacing="0" border="0"  summary="<?php echo $table_title; ?>">
+                <caption><?php echo $captions["title"]["vtm"]; ?><br /><?php echo $captions["title"]["htm"]; ?></caption>
+                <tbody>
+        <?php
+        foreach($global_results as $title=>$result){
+            unset($game_attributes);
+            unset($array_contenders);
+            unset($game_children);
+            unset($game_node);
+            unset($line_attributes);
+            unset($array_contenders);
+            $i = 0;
+
+        while(list( ,$game_node) = each($result)) {
+            $game_attributes = $game_node->attributes();            
+            $array_contenders[$i]['name'] = $game_attributes['htm'];
+
+            list(, $game_children) = each($game_node->children());                    
+
+            if(is_array($game_children)){
+                while(list( ,$line_node) = each($game_children)) {
+                    $line_attributes = $line_node->attributes();                    
+                    $array_contenders[$i]['odds'][] = fractional_odds_calculate($line_attributes);
+                }
+            } else {
+                $line_attributes = $game_children->attributes();
+                $array_contenders[$i]['odds'][] = fractional_odds_calculate($line_attributes);
+            }
+            $i++;           
+        } ?>        <tr>   
+                        <th colspan="3" class="center">
+                            <?php
+                            echo $captions[$title]["vtm"];
+                            if($captions[$title]["htm"] != "")
+                                echo "<br />" . $captions[$title]["htm"];
+                            ?>
+                        </th>
+                    </tr>
+                    <tr>   
+                        <th>&nbsp;</th>            
+                        <th>American Odds</th>            
+                        <th>Fractional Odds</th>            
+                    </tr>
+                    <?php for($i=0;$i<count($array_contenders);$i++) {?>
+                    <tr>
+                        <td>
+                            <?php echo ucwords(strtolower($array_contenders[$i]['name'])); ?>
+                        </td>
+                        <td>                    
+                            <table class="fer" style="width:100%">
+                                <?php $array_odds = $array_contenders[$i]['odds'];
+                                for($j=0;$j<count($array_odds);$j++) { ?>
+                                <tr>
+                                    <td><?php echo ucwords(strtolower($array_odds[$j]['oddsh'])); ?></td>
+                                </tr>
+                                <?php } ?>
+                            </table>
+                        </td>
+                        <td>
+                            <table class="fer" style="width:100%">
+                                <?php $array_odds = $array_contenders[$i]['odds'];
+                                for($j=0;$j<count($array_odds);$j++) { ?>
+                                <tr>                                
+                                    <td><?php echo ucwords(strtolower($array_odds[$j]['fractional_odd'])); ?></td>
+                                </tr>
+                                <?php } ?>
+                            </table>
+                        </td>
+                    </tr>
+            <?php } 
+                unset($result);
+            } //end foreach ?>
+                    <tr>      
+                        <td class="dateUpdated center" colspan="5"><em>Updated <?php echo date("F j, Y");?>. Bet US Racing - Official <a href="http://www.usracing.com/bet-on/american-pharoah">American Pharoah Odds</a>. <br>All odds are fixed odds prices. </em> </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    <?php   } // end if ?>
+
+    <?php $message = "";
+
+    if( $id_league != "" && $result_count>0 ){
+        $message = ob_get_clean();
+    } else{
+  $message = "The odds are currently being updated, please check back shortly. <em> - Updated ".date("F j, Y").". </em>";
+    }
+
+    //echo $message;
+    file_put_contents($OutputPathFile, $message);
+}
+
+function fractional_odds_calculate(& $line_attributes){
+    $row = array();
+    $row['oddsh'] = $line_attributes['oddsh'];
+    $oodsh = intval($line_attributes['oddsh']);
+
+    if(intval($oodsh)>0){
+       $row['fractional_odd'] = float2rat($oodsh/100);     
+    } else if($oodsh<0) {
+        $row['fractional_odd'] = float2rat(100/(-$oodsh));
+    } else {
+        $row['fractional_odd'] = $line_attributes['oddsh'];   
+    }
+
+    return $row;
+}
+
+function float2rat($n, $tolerance = 1.e-6) {
+    $h1=1; $h2=0;
+    $k1=0; $k2=1;
+    $b = 1/$n;
+    do {
+        $b = 1/$b;
+        $a = floor($b);
+        $aux = $h1; $h1 = $a*$h1+$h2; $h2 = $aux;
+        $aux = $k1; $k1 = $a*$k1+$k2; $k2 = $aux;
+        $b = $b-$a;
+    } while (abs($n-$h1/$k1) > $n*$tolerance);
+
+    return "$h1/$k1";
+}
